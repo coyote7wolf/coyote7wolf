c9f94b5bb505f844b76fbc61ab4d395de7a83ba0
wolf04 <mickey985ha@gmail.com>
2025-11-24 10:50:15 +0800

    ✨ feat(JavaSpringBootUserserviceApplication): add JavaSpringBootUserserviceApplication class

---
+++ src/java/java-spring-boot_user-service/src/main/java/com/userservice/java_spring_boot_user_service/JavaSpringBootUserserviceApplication.java
@@ -0,0 +1,12 @@
+package com.userservice.java_spring_boot_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootUserserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JavaSpringBootUserserviceApplication.class, args);
+  }
+}
